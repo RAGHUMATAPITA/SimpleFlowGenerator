d8ba5beec7dfd9448d9c77c707a9f91cbcccf81c
Ron Belmont <belmonrj@gmail.com>
2015-11-30 16:25:49 -0700

    Attempted function arrays, still painfully slow

--- flow_tree.C
+++ flow_tree.C
@@ -20,7 +20,7 @@ double width = 0.5;
 int dostuff(const int, TTree*); // function prototype...
 
 
-const int maxmult = 5000;
+const int maxmult = 100;
 
 int d_mult;
 float d_psi2;
@@ -28,10 +28,28 @@ int d_charge[maxmult];
 float d_pt[maxmult];
 float d_phi[maxmult];
 
+TF1 *funpt;
+TF1 *funphi[maxmult];
 
 
 int main()
 {
+
+  cout << "setting up functions" << endl;
+
+  funpt = new TF1("funpt","TMath::Exp(-0.35*x)",0.0,2.0); // inverse slope param of 350 MeV
+
+  int number = maxmult;
+  for(int i=0; i<number; i++)
+    {
+      float pt = 2*i/number;
+      float v2 = 0.2*pt;
+      funphi[i] = new TF1("funphi","1 + 2*[0]*TMath::Cos(2*x - [1])",-TMath::Pi(),TMath::Pi());
+      funphi[i]->SetParameter(0,v2);
+    }
+
+  cout << "setting up tree " << endl;
+
 
   TFile *file = new TFile("simpletree.root","recreate");
 
@@ -44,14 +62,16 @@ int main()
   tree->Branch("pt",d_pt,"pt[mult]/F");
   tree->Branch("phi",d_phi,"phi[mult]/F");
 
+  cout << "now starting loop" << endl;
+
   //int nevents = 100000; // way too slow...
   int nevents = 10000;
   for(int i=0; i<nevents; i++)
     {
-      int ntracks = 100;
+      if(i % 1000 == 0) cout << i << " events processed so far " << endl;
+      int ntracks = maxmult; //...
       int ntrk = dostuff(ntracks,tree);
       hmult->Fill(ntrk);
-      if(i % 1000 == 0) cout << i << " events processed so far " << endl;
     }
 
   file->Write();
@@ -70,21 +90,27 @@ int main()
 int dostuff(const int number, TTree *tree)
 {
 
-  vector<int> vec_charge;
-  vector<double> vec_phi;
-  TF1 *funpt = new TF1("funpt","TMath::Exp(-0.35*x)",0.0,2.0); // inverse slope param of 350 MeV
-  TF1 *funphi = new TF1("funphi","1 + 2*[0]*TMath::Cos(2*x - [1])",-TMath::Pi(),TMath::Pi());
   float psi2 = gRandom->Uniform(-TMath::Pi(),TMath::Pi()); // phi range for throw of psi2 for event
-  funphi->SetParameter(1,psi2);
+
+  //cout << "seting parameter for psi2" << endl;
+  for(int i=0; i<number; i++)
+    {
+      funphi[i]->SetParameter(1,psi2);
+    }
+
+  //cout << "main track loop" << endl;
   for(int i=0; i<number; i++)
     {
       int charge = -1;
       double random = gRandom->Rndm();
       if(random>0.5) charge = 1;
-      float pt = funpt->GetRandom(); // slow...
+
+      float pt = funpt->GetRandom();
+
       float v2 = 0.2*pt; // v2 = 0.2 at 1 GeV
-      funphi->SetParameter(0,v2);
-      float phi = funphi->GetRandom(); // very slow...
+      int ptbin = pt*number/2;
+      //cout << pt << " " << ptbin << endl;
+      float phi = funphi[ptbin]->GetRandom();
       // ---
       d_charge[i] = charge;
       d_pt[i] = pt;
