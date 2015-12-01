c25b52765216d6481fbbe4d86bd5cd0fd5cb29dd
Ron Belmont <belmonrj@gmail.com>
2015-12-01 10:49:31 -0700

    Added YZ 4-part stuff

--- plot.C
+++ plot.C
@@ -10,7 +10,7 @@ void plot()
   TH1D *th1d_d2pT = ((TProfile *)file->Get("tp1d_d2pT"))->ProjectionX();
   TH1D *th1d_c2 = ((TProfile *)file->Get("tp1d_c2"))->ProjectionX();
 
-  th1d_v2pT_true->SetMaximum(0.25);
+  th1d_v2pT_true->SetMaximum(0.2);
   th1d_v2pT_true->SetMinimum(0.0);
   th1d_v2pT_true->GetYaxis()->SetTitle("v_{2}, various methods");
   th1d_v2pT_true->GetYaxis()->SetTitleOffset(1.2);
@@ -39,7 +39,21 @@ void plot()
   cout << sqrt(c2) << " " << pow(-c4,0.25) << endl;
   // ---
 
-  TLegend *leg = new TLegend(0.68,0.18,0.88,0.38);
+  // ---
+  TH1D *th1d_YZ_p4pT = ((TProfile *)file->Get("tp1d_YZ_p4pT"))->ProjectionX();
+  TH1D *th1d_YZ_p4 = ((TProfile *)file->Get("tp1d_YZ_p4"))->ProjectionX();
+  float p4YZ = th1d_YZ_p4->GetBinContent(1);
+  float c4YZ = p4YZ - 2.0*c2*c2;
+  if(c4>0){cout << "LOLOMGWTF" << endl; c4 *= -1;}
+  float k4YZ = pow(-c4YZ,0.75);
+  th1d_YZ_p4pT->Scale(1.0/k4YZ);// watch...
+  th1d_YZ_p4pT->SetLineColor(kGreen+2);
+  th1d_YZ_p4pT->Draw("same");
+  cout << sqrt(c2) << " " << pow(-c4YZ,0.25) << endl; // watch...
+  // ---
+
+  //TLegend *leg = new TLegend(0.68,0.18,0.88,0.38);
+  TLegend *leg = new TLegend(0.18,0.68,0.38,0.88);
   leg->AddEntry(th1d_v2pT_true,"truth","elp");
   leg->AddEntry(th1d_v2pT_reco,"v_{2}{#Psi_{2}}","elp");
   leg->AddEntry(th1d_d2pT,"v_{2}{2}","elp");
