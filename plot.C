0a6db2c3d30108b70427af0058170f212a41c587
Ron Belmont <belmonrj@gmail.com>
2015-12-04 18:57:31 -0700

    Added resolution correction and pdf figure to v2pTreco in plot

--- plot.C
+++ plot.C
@@ -1,21 +1,22 @@
 void doit(const char*);
 
+
 void plot()
 {
 
-  doit("linear");
-  doit("flat");
+  doit("400tracks_10000events");
 
 }
+
 
 void doit(const char *flag)
 {
 
   TCanvas *c1 = new TCanvas();
 
-  //TFile *file = TFile::Open("out.root");
   TFile *file = TFile::Open(Form("out_%s.root",flag));
 
+  TH1D *th1d_v2pT_pure = ((TProfile *)file->Get("tp1d_v2pT_pure"))->ProjectionX();
   TH1D *th1d_v2pT_true = ((TProfile *)file->Get("tp1d_v2pT_true"))->ProjectionX();
   TH1D *th1d_v2pT_reco = ((TProfile *)file->Get("tp1d_v2pT_reco"))->ProjectionX();
   TH1D *th1d_YZ_v2pT_reco = ((TProfile *)file->Get("tp1d_YZ_v2pT_reco"))->ProjectionX();
@@ -25,6 +26,9 @@ void doit(const char *flag)
   TH1D *th1d_YZ_d2pT = ((TProfile *)file->Get("tp1d_YZ_d2pT"))->ProjectionX();
   TH1D *helperYZ = (TH1D *)th1d_YZ_d2pT->Clone();
   TH1D *th1d_YZ_W = ((TProfile *)file->Get("tp1d_YZ_W"))->ProjectionX();
+  TH1D *th1d_psi2_tmr = ((TProfile *)file->Get("tp1d_psi2_tmr"))->ProjectionX();
+
+  double resolution = th1d_psi2_tmr->GetBinContent(1);
 
   th1d_v2pT_true->SetMaximum(0.22);
   th1d_v2pT_true->SetMinimum(0.0);
@@ -33,12 +37,15 @@ void doit(const char *flag)
   th1d_v2pT_true->GetXaxis()->SetTitle("p_{T} (GeV/c)");
   th1d_v2pT_true->SetLineColor(kBlack);
   th1d_v2pT_true->Draw();
+  th1d_v2pT_pure->SetLineColor(kBlack);
+  th1d_v2pT_pure->Draw("same");
 
+  th1d_v2pT_reco->Scale(1.0/resolution);
   th1d_v2pT_reco->SetLineColor(kRed);
   th1d_v2pT_reco->Draw("same");
   th1d_YZ_v2pT_reco->Divide(th1d_YZ_W); // weight
   th1d_YZ_v2pT_reco->SetLineColor(kRed);
-  th1d_YZ_v2pT_reco->Draw("same");
+  //th1d_YZ_v2pT_reco->Draw("same"); //  weight produces identical result
 
   float c2 = th1d_c2->GetBinContent(1);
   th1d_d2pT->Scale(1.0/sqrt(c2));
@@ -47,7 +54,7 @@ void doit(const char *flag)
   th1d_YZ_d2pT->Divide(th1d_YZ_W);
   th1d_YZ_d2pT->Scale(1.0/sqrt(c2));
   th1d_YZ_d2pT->SetLineColor(kBlue);
-  th1d_YZ_d2pT->Draw("same");
+  //th1d_YZ_d2pT->Draw("same");
   helper->Scale(2*c2);
   helperYZ->Scale(2*c2);
 
@@ -78,7 +85,7 @@ void doit(const char *flag)
   float k4YZ = pow(-c4YZ,0.75);
   th1d_YZ_p4pT->Scale(1.0/k4YZ);// watch...
   th1d_YZ_p4pT->SetLineColor(kGreen+2);
-  th1d_YZ_p4pT->Draw("same");
+  //th1d_YZ_p4pT->Draw("same");
   cout << sqrt(c2) << " " << pow(-c4YZ,0.25) << endl; // watch...
   // ---
 
@@ -92,5 +99,6 @@ void doit(const char *flag)
   leg->Draw();
 
   c1->Print(Form("comparison_%s.png",flag));
+  c1->Print(Form("comparison_%s.pdf",flag));
 
 }
